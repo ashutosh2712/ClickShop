9b1d0704a10759ef46bb3fbead4d266f768739ec
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-06-09 01:38:26 +0530

    Basic setup and Navbar Design

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ShopCart from "../assets/trolley-cart.png";
+import Admin from "../assets/setting.png";
+import Avatar from "../assets/office-man.png";
+const Navbar = () => {
+  return (
+    <nav className="navContainer">
+      <div className="navLeft">
+        <h1>Clickshop</h1>
+      </div>
+
+      <div className="navCenter">
+        <input
+          type="text"
+          placeholder="Search Your Products"
+          className="navSearch"
+        />
+        <button type="submit" className="btn">
+          SEARCH
+        </button>
+      </div>
+
+      <div className="navRight">
+        <div className="navCart">
+          {/* <i className="fas fa-shopping-cart"></i> */}
+          <img src={ShopCart} alt="cart" className="cartImg" />
+          <p>Cart</p>
+        </div>
+
+        <div className="navAdmin">
+          <img src={Admin} alt="cart" className="cartImg" />
+          <p>Admin</p>
+        </div>
+        <div className="navUser">
+          <img src={Avatar} alt="cart" className="cartImg" />
+          <p>Brock</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
