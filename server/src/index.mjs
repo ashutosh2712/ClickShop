bcbc0ad28e4d00d84a60c32bc614f0dc79904ef9
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-07-08 23:26:24 +0530

    Initial Express Server Setup.

---
+++ server/src/index.mjs
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Express Server started and Listening at ${PORT}`);
+  console.log("http://localhost:3000");
+});
