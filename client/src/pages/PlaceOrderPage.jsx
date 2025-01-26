4b0398759cb5f76dddc43d805c2aa897596e3b69
ashutosh2712 <ashu00619@gmail.com>
2025-01-27 00:03:36 +0530

    Place order page dynamic data

--- client/src/pages/PlaceOrderPage.jsx
+++ client/src/pages/PlaceOrderPage.jsx
@@ -1,72 +1,116 @@
 import React from "react";
-import products from "../data/products";
+import Message from "../components/Message";
 import { Link } from "react-router-dom";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { useSelector } from "react-redux";
 const PlaceOrderPage = () => {
-  return (
-    <div className="placeOrderContainer">
-      <div className="leftPlaceOrderDetails">
-        <div className="placeOrderDetails">
-          <h2>Shipping</h2>
-          <p>
-            <b>Shipping:</b> Las Veags Navada,USA,100001
-          </p>
-        </div>
-        <div className="placeOrderDetails">
-          <h2>Payment Method</h2>
-          <p>
-            <b>Method:</b> PayPal
-          </p>
-        </div>
-        <div className="placeOrderDetails">
-          <h2>Order Items</h2>
-          {products.map((product) => (
-            <div className="placeOrderProductDetails">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="placeOrderProductImg"
-              />
-              <p>{product.name}</p>
-              <p>1 X ${product.price}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+  const cart = useSelector((state) => state.cart);
 
-      <div className="placeOrderSummary">
-        <div className="orderSummaryDetails">
-          <h2>Order Summary</h2>
+  cart.itemsPrice = cart.cartItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
+  );
+  cart.shippingPrice = cart.itemsPrice > 100 ? 0 : 25;
+  cart.taxPrice = Number((0.15 * cart.itemsPrice).toFixed(2));
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2);
+
+  const placeOrderHandler = () => {
+    console.log("object");
+  };
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <div className="placeOrderContainer">
+        <div className="leftPlaceOrderDetails">
+          <div className="placeOrderDetails">
+            <h2>Shipping</h2>
+            <p>
+              <b>Shipping:</b> {cart.shippingAddress.address},{" "}
+              {cart.shippingAddress.city}, {cart.shippingAddress.country},{" "}
+              {cart.shippingAddress.postalCode}
+            </p>
+          </div>
+          <div className="placeOrderDetails">
+            <h2>Payment Method</h2>
+            <p>
+              <b>Method:</b> {cart.paymentMethod}
+            </p>
+          </div>
+          <div className="placeOrderDetails">
+            <h2>Order Items</h2>
+            {cart.cartItems.length === 0 ? (
+              <Message className="warningMessage">
+                <p>{"*Your Cart is empty"}</p>
+                <Link to="/" className="returnLink goBackCart">
+                  <button className="btn-cart">GO BACK</button>
+                </Link>
+              </Message>
+            ) : (
+              <>
+                {" "}
+                {cart.cartItems.map((product) => (
+                  <Link
+                    to={`/products/${product.product}`}
+                    className="placeOrderProductDetails"
+                    key={product.product}
+                  >
+                    <img
+                      src={`http://localhost:3000/uploads/` + product.image}
+                      alt={product.name}
+                      className="placeOrderProductImg"
+                    />
+                    <p>{product.name}</p>
+                    <p>
+                      {product.qty} X ${product.price}
+                    </p>
+                  </Link>
+                ))}
+              </>
+            )}
+          </div>
         </div>
-        <div className="orderSummaryDetails">
-          <p>
-            <b>Items:</b>
-          </p>
-          <p>$1200</p>
-        </div>
-        <div className="orderSummaryDetails">
-          <p>
-            <b>Shipping:</b>
-          </p>
-          <p>$120</p>
-        </div>
-        <div className="orderSummaryDetails">
-          <p>
-            <b>Taxes:</b>
-          </p>
-          <p>$20</p>
-        </div>
-        <div className="orderSummaryDetails">
-          <p>
-            <b>Total:</b>
-          </p>
-          <p>$3700</p>
-        </div>
-        <div className="orderSummaryDetails">
-          <Link to="/order">
-            <button type="submit" className="btn-cart">
+
+        <div className="placeOrderSummary">
+          <div className="orderSummaryDetails">
+            <h2>Order Summary</h2>
+          </div>
+          <div className="orderSummaryDetails">
+            <p>
+              <b>Items:</b>
+            </p>
+            <p>${cart.itemsPrice}</p>
+          </div>
+          <div className="orderSummaryDetails">
+            <p>
+              <b>Shipping:</b>
+            </p>
+            <p>${cart.shippingPrice}</p>
+          </div>
+          <div className="orderSummaryDetails">
+            <p>
+              <b>Taxes:</b>
+            </p>
+            <p>${cart.taxPrice}</p>
+          </div>
+          <div className="orderSummaryDetails">
+            <p>
+              <b>Total:</b>
+            </p>
+            <p>${cart.totalPrice}</p>
+          </div>
+          <div className="orderSummaryDetails">
+            <button
+              type="submit"
+              className="btn-cart"
+              onClick={placeOrderHandler}
+            >
               Place Order
             </button>
-          </Link>
+          </div>
         </div>
       </div>
     </div>
